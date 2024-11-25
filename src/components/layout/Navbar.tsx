1eeebc8268f433f5f419a950add60339d410d2b3
Marcos Valenzuela <87524702+Dantalian5@users.noreply.github.com>
2024-11-25 11:08:39 +0100

    Add initial navbar setup

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,4 +1,27 @@
+import ThemeSwitch from "@/components/commons/ThemeSwitch";
+
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex w-full items-center justify-between bg-background-neutral">
+      <div className="relative w-fit overflow-hidden rounded-r-2xl bg-primary p-[22px] before:absolute before:inset-0 before:top-1/2 before:z-10 before:rounded-tl-2xl before:bg-primary-light before:content-['']">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="28"
+          height="26"
+          className="relative z-20"
+        >
+          <path
+            fill="#FFF"
+            fillRule="evenodd"
+            d="M20.513 0C24.965 2.309 28 6.91 28 12.21 28 19.826 21.732 26 14 26S0 19.826 0 12.21C0 6.91 3.035 2.309 7.487 0L14 12.9z"
+          />
+        </svg>
+      </div>
+      <div>
+        <ThemeSwitch />
+      </div>
+      <div></div>
+    </div>
+  );
 }
 export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,34 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["Spartan", "sans-serif"],
+    },
+    colors: {
+      background: {
+        light: "#F8F8FB",
+        dark: "#141625",
+        neutral: "#373b53",
+      },
+      danger: {
+        DEFAULT: "#EC5757",
+        light: "#9277FF",
+      },
+      primary: {
+        DEFAULT: "#7C5DFA",
+        light: "#9277FF",
+      },
+      secondary: {
+        DEFAULT: "#1E2139",
+        light: "#252945",
+      },
+      foreground: {
+        100: "#DFE3FA",
+        200: "#888EB0",
+        300: "#7E88C3",
+        400: "#0C0E16",
+      },
+    },
     extend: {},
   },
   plugins: [],
